c58c3f807723eed66191e534e2ebb1aea588f72d
Stevie Michalik <michaliksj21@gcc.edu>
2025-03-08 13:19:35 -0500

    Updates to Main.java to run the experiments

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,83 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
-    final static int NUM_ROWS = 10;
-    final static int NUM_COLS = 10;
-    final static int NUM_PIECES = (NUM_ROWS*NUM_COLS)/4;
+    final static int NUM_ROWS = 7;
+    final static int NUM_COLS = 7;
+    final static int NUM_PIECES = (NUM_ROWS*NUM_COLS)/5;
+    final static int NUM_EXPERIMENT_ITERATIONS = 5;
+    final static int VERBOSITY = 0;
 
     public static void main(String[] args) {
-        ArrayList<Piece> pieces= new ArrayList<>();
-        HashMap<String, Integer> pieceCount = new HashMap<>();
 
+        // Run the experiments with Backtracking Search
+        int numSolutionsFoundBacktracking = 0;
+        long[] solutionTimeBacktracking = new long[NUM_EXPERIMENT_ITERATIONS];
+
+        for (int i = 0; i < NUM_EXPERIMENT_ITERATIONS; i++) {
+            ArrayList<Piece> pieces = generatePieces();
+            Board board = new Board(NUM_ROWS,NUM_COLS);
+            TetrisDomains domains = new TetrisDomains(board, pieces);
+
+            long startTime = System.nanoTime();
+            TetrisDomains solution = BacktrackingSearch.backtrack(domains, new HashSet<>(), VERBOSITY);
+            long estimatedTime = System.nanoTime() - startTime;
+            solutionTimeBacktracking[i] = estimatedTime;
+
+            if (solution != null) {
+                numSolutionsFoundBacktracking++;
+            }
+        }
+
+        printEvaluationMetrics(numSolutionsFoundBacktracking, solutionTimeBacktracking);
+
+        // The same, just with Conflict Directed Backjumping
+        int numSolutionsFoundConflictDirectedBackjumping = 0;
+        long[] solutionTimeConflictDirectedBackjumping = new long[NUM_EXPERIMENT_ITERATIONS];
+
+        for (int i = 0; i < NUM_EXPERIMENT_ITERATIONS; i++) {
+            ArrayList<Piece> pieces = generatePieces();
+            Board board = new Board(NUM_ROWS,NUM_COLS);
+            TetrisDomains domains = new TetrisDomains(board, pieces);
+            ConflictDirectedBackjumping cbj = new ConflictDirectedBackjumping(null);
+
+            long startTime = System.nanoTime();
+            TetrisDomains solution = cbj.backtrack(domains, new LinkedHashSet<>(), new HashMap<>(), VERBOSITY);
+            long estimatedTime = System.nanoTime() - startTime;
+            solutionTimeConflictDirectedBackjumping[i] = estimatedTime;
+
+            if (solution != null) {
+                numSolutionsFoundConflictDirectedBackjumping++;
+            }
+        }
+
+        printEvaluationMetrics(numSolutionsFoundConflictDirectedBackjumping, solutionTimeConflictDirectedBackjumping);
+    }
+
+    private static void printEvaluationMetrics (int solutionsFound, long[] backtrackingTimes) {
+        float percentOfSolutions = (float) (solutionsFound /NUM_EXPERIMENT_ITERATIONS)*100;
+        System.out.printf("Percentage of solutions found: %.2f\n", percentOfSolutions);
+
+        long totalTime = 0;
+        for (int i = 0; i < backtrackingTimes.length; i++) {
+            totalTime += backtrackingTimes[i];
+        }
+
+        float avgTime = (float) (totalTime/NUM_EXPERIMENT_ITERATIONS)/1000000;
+        System.out.printf("Average Calculation Time: %.2f ms\n", avgTime);
+    }
+
+    private static ArrayList<Piece> generatePieces() {
+        ArrayList<Piece> pieces = new ArrayList<>();
         Random rand = new Random();
 
         for(int i = 0; i < NUM_PIECES; i++){
             Piece piece = generatePiece(rand.nextInt(7));
-            int count = pieceCount.getOrDefault(piece.toString(), 0) + 1;
-            pieceCount.put(piece.toString(), count);
             pieces.add(piece);
         }
 
-
-        System.out.println("Trying a board of size " + NUM_ROWS + "x" +NUM_COLS + " with:");
-        pieceCount.forEach((key, value) -> {
-            if(value == 1) System.out.println(value + " " + key);
-            else System.out.println(value + " " + key + "s");
-        });
-
-
-        Board board = new Board(NUM_ROWS,NUM_COLS);
-        TetrisDomains domains = new TetrisDomains(board, pieces);
-        TetrisDomains solution = BacktrackingSearch.backtrack(domains, new HashSet<>());
-
-        if(solution == null){
-            System.out.println("Didn't work");
-        }else{
-            System.out.println(solution.domains);
-        }
+        return pieces;
     }
+
     private static Piece generatePiece(int piece){
         return switch (piece) {
             case 1 -> new I();
